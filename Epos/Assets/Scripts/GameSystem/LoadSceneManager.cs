1b3d031a9a3f707889e6d0b150c64f6c56706a61
Seungjae <sj@SJui-MacBookPro.local>
2024-10-08 00:42:20 +0900

    [Program] loadscene 수정.

--- Epos/Assets/Scripts/GameSystem/LoadSceneManager.cs
+++ Epos/Assets/Scripts/GameSystem/LoadSceneManager.cs
@@ -11,16 +11,19 @@ namespace GameSystem
 {
     public class LoadSceneManager : Singleton<LoadSceneManager>
     {
+        private string _sceneName = string.Empty;
+        private string _loadSceneName = string.Empty;
+        private Fade _fade = null;
+        
         private bool _isLoad = false;
         
         protected override void Initialize()
         {
             DontDestroyOnLoad(this);
 
-            _isLoad = false;
+            Reset();
         }
 
-        // 임시
         public async UniTask LoadSceneAsync(string name)
         {
             await UniTask.Yield();
@@ -28,54 +31,70 @@ namespace GameSystem
             if (_isLoad)
                 return;
             
-            Debug.Log(_isLoad);
-
             _isLoad = true;
+            _loadSceneName = name;
+            _sceneName = SceneManager.GetActiveScene().name;
             
-            var currSceneName = SceneManager.GetActiveScene().name;
-            Fade fade = null;
-            Debug.Log(currSceneName);
             AsyncOperationHandle<SceneInstance> sceneInstance =
                 Addressables.LoadSceneAsync("Assets/Scenes/Load.unity", LoadSceneMode.Additive);
-            sceneInstance.Completed +=
-                (handle) =>
-                {
-                    foreach (var rootGameObj in handle.Result.Scene.GetRootGameObjects())
-                    {
-                        if(!rootGameObj)
-                            continue;
-                        
-                        fade = rootGameObj.GetComponentInChildren<Fade>();
-                        if (fade != null)
-                            break;
-                    }
-                };
+            sceneInstance.Completed += LoadFade;
+        }
 
-            await UniTask.WaitUntil(() => fade != null);
-            await UniTask.WaitForEndOfFrame(this);
+        private void LoadFade(AsyncOperationHandle<SceneInstance> handle)
+        {
+            foreach (var rootGameObj in handle.Result.Scene.GetRootGameObjects())
+            {
+                if(!rootGameObj)
+                    continue;
+                        
+                var fade = rootGameObj.GetComponentInChildren<Fade>();
+                if (fade == null)
+                    continue;
+                
+                FadeOutAsync(fade).Forget();
+
+                break;
+            }
+        }
+
+        private async UniTask FadeOutAsync(Fade fade)
+        {
+            _fade = fade;
             
-            fade?.Out(
+            _fade?.Out(
                 () =>
                 {
                     AsyncOperationHandle<SceneInstance> sceneInstance =
-                        Addressables.LoadSceneAsync($"Assets/Scenes/{name}.unity", LoadSceneMode.Additive);
-                    sceneInstance.Completed +=
-                        (handle) =>
-                        {
-                            SceneManager.UnloadSceneAsync(currSceneName);
-            
-                            fade.In(
-                                () =>
-                                {
-                                    SceneManager.UnloadSceneAsync("Load");
-                                });
-                          
-                            _isLoad = false;
-                        };
+                        Addressables.LoadSceneAsync($"Assets/Scenes/{_loadSceneName}.unity", LoadSceneMode.Additive);
+                    sceneInstance.Completed += UnLoadScene;
                 });
+        }
 
-            // _isLoad = false;
-            Debug.Log("End Load Scene = " + name);
+        private void UnLoadScene(AsyncOperationHandle<SceneInstance> handle)
+        {
+            SceneManager.UnloadSceneAsync(_sceneName);
+
+            FadeInAsync().Forget();
+        }
+        
+        private async UniTask FadeInAsync()
+        {
+            await UniTask.Yield();
+            
+            _fade?.In(
+                () =>
+                {
+                    SceneManager.UnloadSceneAsync("Load");
+
+                    Reset();
+                });
+        }
+
+        private void Reset()
+        {
+            _isLoad = false;
+            _sceneName = string.Empty;
+            _loadSceneName = string.Empty;
         }
     }
 }
