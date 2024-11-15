b2f08ea5af175d047c3910fab17832cb76bbd350
HanSeungjae-OCTOPO <octopo-041@OCTOPO-041ui-MacBookPro.local>
2024-11-15 17:48:47 +0900

    [Program] 전투 UI 작업 중.

--- Epos/Assets/Scripts/UI/Panels/BattleForces.cs
+++ Epos/Assets/Scripts/UI/Panels/BattleForces.cs
@@ -2,14 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+using Parts;
+
 namespace UI.Panels
 {
     public class BattleForces : Panel<BattleForces.Data>
     {
         public class Data : Base
         {
-            public Parts.Forces LeftForces = null;
-            public Parts.Forces RightForces = null;
+            public Forces LeftForces = null;
+            public Forces RightForces = null;
         }
 
         public override Panel<Data> Initialize(Data data)
---
+++ Epos/Assets/Scripts/UI/Parts/Combatant.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UI.Parts
+{
+    public class BattleCombatant : Part
+    {
+        [SerializeField] 
+        private int position = 0;
+    }
+}
+
