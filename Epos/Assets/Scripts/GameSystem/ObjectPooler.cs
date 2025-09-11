94111316e1d4278ef07a7a8b383a4aa1cb90eef8
Seungjae <126159238+purpism7@users.noreply.github.com>
2025-09-11 18:36:05 +0900

    object pooler parent 변경.

--- Epos/Assets/Scripts/GameSystem/ObjectPooler.cs
+++ Epos/Assets/Scripts/GameSystem/ObjectPooler.cs
@@ -30,7 +30,7 @@ namespace GameSystem
             if (iPoolable == null)
                 return;
 
-            //iPoolable.Transform.SetParent(transform);
+            iPoolable.Transform.SetParent(transform);
             iPoolable.Transform.SetActive(false);
         }
 
